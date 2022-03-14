ab5592573ccfb9fee38cf4621cbed1d275388944
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-03-14 16:19:10 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ websockify -D --web=/usr/share/novnc/ --cert=/home/debian/novnc.pem 6080 localho
 echo Downloading Windows Disk...
 wget http://mirror.myfahim.com/centos/7.9.2009/isos/x86_64/CentOS-7-x86_64-Minimal-2009.iso
 read -p "echo Enter Hard Disk Size 20G recomended: " CRP
-qemu img create centos7.vmdk $CRP
+qemu-img create centos7.vmdk $CRP
 echo "Centos 7 Core x64 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
