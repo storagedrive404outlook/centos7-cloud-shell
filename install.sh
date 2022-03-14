4fe0a08c91573074f77fd895db29c42a7b534973
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-03-14 16:07:38 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,28 +8,13 @@ sudo apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 qemu qemu-system curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
-wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
-mv bionic-server-cloudimg-amd64.img ubuntu-18.04-server-cloudimg-amd64.img
-sudo apt-get install cloud-image-utils
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-
-cat >user-data <<EOF
-#cloud-config
-password: asdfqwer
-chpasswd: { expire: False }
-ssh_pwauth: True
-EOF
-
-cloud-localds user-data.img user-data
-
-# user-data.img MUST come after the rootfs. 
-qemu-system-x86_64 \
--drive file=ubuntu-18.04-server-cloudimg-amd64.img,format=qcow2 \
--drive file=user-data.img,format=raw -vnc :1 -m 8192
-echo "Ubuntu 18.04 TLS Server x64 Lite On Google Colab"
+wget http://mirror.myfahim.com/centos/7.9.2009/isos/x86_64/CentOS-7-x86_64-Minimal-2009.iso
+read -p "echo Enter Hard Disk Size 20G recomended: " CRP
+qemu img create centos7.vmdk $CRP
+echo "Centos 7 Core x64 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by Storagedrive404outlook
-sudo qemu-system-x86_64 -vnc :0 -hda ubuntu.img  -smp cores=2  -m 8192M  > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -cdrom CentOS-7-x86_64-Minimal-2009.iso -hda centos7.vmdk  -smp cores=2  -m 8192M  > /dev/null 2>&1
