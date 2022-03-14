9efa8dc3b2cee2cb06510408ce17d77d56b7b002
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-03-14 16:10:45 +0600

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 5900 &>/dev/null &
+echo Please wait for installing...
+sudo apt update -y > /dev/null 2>&1
+echo "Installing QEMU (2-3m)..."
+sudo apt install qemu-system-x86 qemu qemu-system curl -y > /dev/null 2>&1
+echo "Centos 7 Core x64 Lite On Google Colab"
+echo Your VNC IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Note: Use Right-Click Or Ctrl+C To Copy"
+echo "Please Keep Colab Tab Open, Maximum Time 12h"
+echo Script by Storagedrive404outlook
+sudo qemu-system-x86_64 -vnc :0 -hda centos7.vmdk  -smp cores=2  -m 8192M  > /dev/null 2>&1
