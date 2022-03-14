fd1779d7464ba68525d5070824fe6788e96cd6f4
storagedrive404outlook <99749949+storagedrive404outlook@users.noreply.github.com>
2022-03-14 16:16:26 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
 sudo apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
-sudo apt install qemu-system-x86 qemu qemu-system curl -y > /dev/null 2>&1
+sudo apt install qemu-system-x86 qemu qemu-system curl novnc python3-websockif -y > /dev/null 2>&1
+openssl req -x509 -nodes -newkey rsa:3072 -keyout novnc.pem -out novnc.pem -days 3650
+websockify -D --web=/usr/share/novnc/ --cert=/home/debian/novnc.pem 6080 localhost:5901
 echo "Centos 7 Core x64 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
